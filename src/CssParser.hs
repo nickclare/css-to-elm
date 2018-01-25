9f202fda189916246a236b50ac9053f4fe30836a
Nick Clare <nicholas.clare@barclayscapital.com>
2018-01-25 11:33:57 +0200

    Work on keywords and selectors

--- src/CssParser.hs
+++ src/CssParser.hs
@@ -3,13 +3,13 @@
 
 module CssParser where
 
+import           Control.Monad
 import           Control.Applicative
 import           Data.Attoparsec.Text
 import           Data.Char
-import           Data.Text            hiding (concat, takeWhile)
+import           Data.Text            hiding (concat, count, map, takeWhile, toLower, toUpper)
 import           Prelude              hiding (takeWhile)
-
-import qualified Debug.Trace          as Trace
+import           Numeric              (showHex)
 
 data CssRule
   = QualifiedRule
@@ -20,18 +20,22 @@ newtype Stylesheet =
   Stylesheet [CssRule]
   deriving (Show)
 
+data Selector = Simple SimpleSelector | Other
+
+data SimpleSelector = IdSelector String | ClassSelector String | AttribSelector String | PseudoSelector String | Negation SimpleSelector
+
 -- Lexer rules
 comment :: Parser ()
-comment = (string "/*" *> manyTill anyChar (string "*/")) >> return ()
+comment = void (string "/*" *> manyTill anyChar (string "*/"))
 
 whiteSpace :: Parser ()
-whiteSpace = many (comment <|> (skip isSpace >> skipWhile isSpace)) >> return ()
+whiteSpace = void $ many (comment <|> (skip isSpace >> skipWhile isSpace))
 
 identToken :: Parser String
 identToken = do
   hyphen <- option "" (char '-' >> return "-")
-  lead <- satisfy (\c -> c /= '-')
-  restT <- (takeWhile (\x -> isPrint x || x == '_'))
+  lead <- satisfy (/= '-')
+  restT <- takeWhile (\x -> isPrint x || x == '_')
   let rest = unpack restT
   return $ hyphen ++ lead : rest
 
@@ -46,24 +50,83 @@ quotedString = do
       d <- char '\\'
       c <- satisfy $ inClass "\\\"0nrvtbf" -- this may perform poorly
       return [d, c]
-    nonEscape = (\x -> [x]) <$> (satisfy $ notInClass "\\\"\0\n\r\v\t\b\f")
+    nonEscape = (: []) <$> satisfy (notInClass "\\\"\0\n\r\v\t\b\f")
     character = nonEscape <|> escape
 
-stylesheet :: Parser Stylesheet
-stylesheet = many stylesheetElement >>= \es -> return $ Stylesheet es
+-- stylesheet :: Parser Stylesheet
+-- stylesheet = many stylesheetElement >>= \es -> return $ Stylesheet es
+--   where
+--     stylesheetElement =
+--       Trace.trace "stylesheetElement" $ do
+--         _ <- whiteSpace
+--         e <- atRule <|> qualifiedRule
+--         _ <- whiteSpace
+--         return e
+
+newlineOrSpace :: Parser ()
+newlineOrSpace = option () $ cr <|> ws
   where
-    stylesheetElement =
-      Trace.trace "stylesheetElement" $ do
-        _ <- whiteSpace
-        e <- atRule <|> qualifiedRule
-        _ <- whiteSpace
-        return e
+    cr = void (string "\r\n")
+    ws = void (satisfy (inClass " \t\r\n\f"))
 
-qualifiedRule :: Parser CssRule
-qualifiedRule =
-  Trace.trace
-    ("qualified rule")
-    (string "qualifiedRule" >> return QualifiedRule)
 
-atRule :: Parser CssRule
-atRule = string "@atRule" >> return AtRule
+mediaKeyword :: Parser ()
+mediaKeyword = atKeyword "media"
+
+importKeyword :: Parser ()
+importKeyword = atKeyword "import"
+
+atKeyword :: String -> Parser ()
+atKeyword kw = char '@' *> mapM_ keywordChar kw
+
+keywordChar :: Char -> Parser ()
+keywordChar c =
+  simpleChar <|> escapedChar <|> if not (validHex c) then hexChar else fail "keywordChar"
+  where
+    simpleChar = void (char $ toLower c) <|> void (char $ toUpper c)
+    escapedChar = do
+      _ <- char '\\'
+      _ <- zeros
+      _ <- string (pack (toHex $ toLower c)) <|> string (pack (toHex $ toUpper c))
+      newlineOrSpace
+      return ()
+    hexChar = do
+      _ <- char '\\'
+      _ <- simpleChar
+      return ()
+    validHex ch =
+      let
+        k = toLower ch
+      in
+        k >= 'a' && k <= 'f'
+
+    zeros = count 4 (option () $ void (char '0'))
+    toHex ch = showHex (ord ch) ""
+
+
+-- Selectors
+-- selector :: Parser Selector
+-- selector = simpleSelectorSequence `sepBy1` combinator
+--   where
+--     combinator = (char '+' <|> char '>' <|> char '~'  <* whiteSpace
+
+simpleSelector :: Parser Selector
+simpleSelector = elSelector <|> (Simple <$> bareSelector)
+  where
+    elSelector = do
+      el <- identToken
+
+      return Other
+    bareSelector =
+          IdSelector <$> (char '#' *> elementIdentifier)
+      <|> ClassSelector <$> (char '.' *> identToken)
+      <|> AttribSelector <$> (char '[' *> attribContents <* char ']' )
+      <|> PseudoSelector <$> do
+        c1 <- unpack <$> string ":"
+        c2 <- option "" (unpack <$> string ":")
+        sel <- identToken
+        return $ c1 ++ c2 ++ sel
+
+    attribContents = unpack <$> takeWhile (/= ']')
+
+elementIdentifier = unpack <$> string "identifier" -- TODO
